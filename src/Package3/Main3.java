e54bd85c82121b6be25ff7d330729bec9a6d82e4
Riccardo Duxenneuner <riccardo@duxi.at>
2018-11-08 13:56:59 +0100

    Package 3

---
+++ src/Package3/Main3.java
@@ -0,0 +1,14 @@
+package Package3;
+
+public class Main3 {
+
+    public static void main (String [] args)
+    {
+       String name = "Riccardo Duxenneuner";
+       int alter = 26;
+
+        System.out.println(name);
+        System.out.println(alter);
+    }
+
+}
