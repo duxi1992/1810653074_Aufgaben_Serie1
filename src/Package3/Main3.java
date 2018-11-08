237c85b69591f555d6449e2f22f28361527ee11c
Riccardo Duxenneuner <riccardo@duxi.at>
2018-11-08 14:11:03 +0100

    Aufgabe 5

--- src/Package3/Main3.java
+++ src/Package3/Main3.java
@@ -2,20 +2,23 @@ package Package3;
 
 public class Main3 {
 
-    public static void main (String [] args)
-    {
-       String name = "Riccardo Duxenneuner";
+    public static void main(String[] args) {
+        String name = "Riccardo Duxenneuner";
 
-       int alter = 26;
+        int alter = 26;
 
         System.out.println(name);
         System.out.println(alter);
 
-    // Aufgabe 4:
+        // Aufgabe 4:
         //Erstellung Variable vom Typ String und verkürzung auf fhKufsteinTirol
         // Durch Zuweisung von final kann die Variable nicht mehr geändert werden; Umbenneung auf FH_KUFSTEIN_TIROL
 
-       final String   FH_KUFSTEIN_TIROL = "Fachhochschule Kufstein Tirol";
+        final String FH_KUFSTEIN_TIROL = "Fachhochschule Kufstein Tirol";
+        // fi + autocomplete; Strg + autocomplete + eine Bezeichnung hinzufügen, mehrmals Enter drücken und dann ein ; hinzufügen, STRG-ALT L drücken, und Ereignisse beobachten
+
+        final String bezeichnung;
+
 
     }
 
