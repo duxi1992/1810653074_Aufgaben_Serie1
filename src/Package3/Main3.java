28e2018bf8bed2fa4d8b1805fff0623e8b46c51f
Riccardo Duxenneuner <riccardo@duxi.at>
2018-11-08 14:06:36 +0100

    Aufgabe 4

--- src/Package3/Main3.java
+++ src/Package3/Main3.java
@@ -5,10 +5,18 @@ public class Main3 {
     public static void main (String [] args)
     {
        String name = "Riccardo Duxenneuner";
+
        int alter = 26;
 
         System.out.println(name);
         System.out.println(alter);
+
+    // Aufgabe 4:
+        //Erstellung Variable vom Typ String und verkürzung auf fhKufsteinTirol
+        // Durch Zuweisung von final kann die Variable nicht mehr geändert werden; Umbenneung auf FH_KUFSTEIN_TIROL
+
+       final String   FH_KUFSTEIN_TIROL = "Fachhochschule Kufstein Tirol";
+
     }
 
 }
