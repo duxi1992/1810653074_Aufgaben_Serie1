9bbaf7aec6084e8ab943bb6f2d7ca821afd83094
Riccardo Duxenneuner <riccardo@duxi.at>
2018-11-08 13:44:22 +0100

    Mit Kommentaren

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
+
+
+// erstellt eine Klasse "Main"
 public class Main {
 
+    // erstellt eine neue Methode "main"
+
     public static void main(String[] args) {
+        // Gibt Hello World, Riccardo Duxenneuner, und eine leere Zeile aus, bei der Verwendung von println wird immer in eine neue Zeile geschrieben.
         System.out.println("Hello World!");
+        System.out.println("Riccardo Duxenneuner");
+        System.out.println();
     }
+
+
 }
+
--- src/Package2/Main2.java
+++ src/Package2/Main2.java
@@ -1,4 +1,10 @@
+// erstellt ein neues Package
 package Package2;
-
+// erstellt eine neue klasse "Main2"
 public class Main2 {
+    //erstellt eine neue Methode " Main"
+    public static void main (String[] args){
+        // gibt " FH Kufstein Tirol" auf der Konsole aus
+        System.out.println("FH Kufstein Tirol");
+    }
 }
